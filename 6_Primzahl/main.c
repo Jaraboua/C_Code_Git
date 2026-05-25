9e8442870a6ebc2035f8770a6671278020cdc97e
elmostafajarboua@gmail.com <mostafaothregensburg@gmail.com>
2026-05-25 11:47:21 +0200

    Die Aufgabe 6 ist Komplett gelöst

--- 6_Primzahl/main.c
+++ 6_Primzahl/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+// Funktionen Prototyp
+int isPrime( int num );
+void printPrimes( int lowerLimit , int upperLimit );
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int lowerLimit , upperLimit ;
+    printf("Enter the lower and upper limit to list primes:");
+    scanf("%d%d", &lowerLimit, &upperLimit );
+
+    printPrimes( lowerLimit , upperLimit );
+
+    return 0 ;
+
+
+}
+
+void printPrimes( int lowerLimit , int upperLimit )
+{
+    printf("All prime number between %d to %d are:", lowerLimit , upperLimit );
+    while( lowerLimit <= upperLimit )
+    {
+        if( isPrime(lowerLimit))
+        {
+            printf("%d\n" , lowerLimit );
+        }
+        lowerLimit++ ;
+    }
+}
+
+int isPrime( int num )
+{
+    int i ;
+    for( i = 2 ; i <= num/2 ; i++ )
+    {
+        if( num % i == 0 )
+        {
+            return 0 ;  // nicht Prim
+        }
+    }
+    return 1 ;   // Prim
 }
